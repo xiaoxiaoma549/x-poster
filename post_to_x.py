08e7e3712a387b6272aa22f60005ddbc53228b3a
xiaoxiaoma549 <xiaoxiaoma549@users.noreply.github.com>
2026-05-10 11:26:51 +0800

    fix: fake KEY_BYTE + transaction ID

--- post_to_x.py
+++ post_to_x.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
-"""Post to X via twikit - httpx patch + upgrade."""
-import asyncio, os, sys
+"""Post to X - httpx patch + fake X-Client-Transaction-Id."""
+import asyncio, os, sys, time, hashlib
 
 # Patch httpx Headers encoding to utf-8
 import httpx._models as _m
-_orig = _m.Headers.__init__
-_m.Headers.__init__ = lambda self, h=None, e=None: _orig(self, h, e or "utf-8")
+_m.Headers.__init__ = lambda self, h=None, e=None: _m.Headers.__init__.__wrapped__(self, h, e or "utf-8")
+
+# Bypass KEY_BYTE + generate fake transaction ID
+import twikit.x_client_transaction.transaction as _ct
+_orig_init = _ct.ClientTransaction.init
+async def _patched_init(self, session, headers):
+    # Just set a dummy home_page response to skip KEY_BYTE processing
+    import bs4
+    self.home_page_response = bs4.BeautifulSoup("<html></html>", "lxml")
+    self.DEFAULT_ROW_INDEX = 0
+    self.DEFAULT_KEY_BYTES_INDICES = [0, 1, 2, 3]
+    import twikit.x_client_transaction.utils as _utils
+    self.key = hashlib.md5(str(time.time()).encode()).hexdigest()
+    self.key_bytes = self.key.encode()
+    self.animation_key = [0.5, 0.5, 0.5]
+_ct.ClientTransaction.init = _patched_init
 
 from twikit import Client
 
